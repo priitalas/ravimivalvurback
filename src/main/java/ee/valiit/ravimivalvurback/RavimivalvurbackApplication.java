5643df22b54f687c4bda4ee15c539634cbf747cc
opilane <priitalas12@gmail.com>
2024-04-09 12:52:38 +0300

    Initial commit

---
+++ src/main/java/ee/valiit/ravimivalvurback/RavimivalvurbackApplication.java
@@ -0,0 +1,13 @@
+package ee.valiit.ravimivalvurback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavimivalvurbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavimivalvurbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/valiit/ravimivalvurback/RavimivalvurbackApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.valiit.ravimivalvurback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RavimivalvurbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
