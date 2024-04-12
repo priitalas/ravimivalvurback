0276eb558ed9e14cc304a815bf4ce8cbca637e80
Margit Toovere <margit.toovere@gmail.com>
2024-04-12 13:50:34 +0300

    Registration Response valmis

--- src/main/java/ee/valiit/ravimivalvurback/domain/user/ContactMapper.java
+++ src/main/java/ee/valiit/ravimivalvurback/domain/user/ContactMapper.java
@@ -8,7 +8,6 @@ public interface ContactMapper {
     @Mapping(source = "firstName", target = "firstName")
     @Mapping(source = "lastName", target = "lastName")
     @Mapping(source = "email", target = "email")
-    @Mapping(source = "", target = "")
     Contact toContact(RegistrationRequest registrationRequest);
 }
 
