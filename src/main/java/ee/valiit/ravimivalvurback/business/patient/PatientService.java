1aa844d9ccdc151410e8ec27aeefc605c3e61092
opilane <priitalas12@gmail.com>
2024-04-24 10:56:56 +0300

    Patient view WIP

--- src/main/java/ee/valiit/ravimivalvurback/business/patient/PatientService.java
+++ src/main/java/ee/valiit/ravimivalvurback/business/patient/PatientService.java
@@ -2,11 +2,16 @@ package ee.valiit.ravimivalvurback.business.patient;
 
 import ee.valiit.ravimivalvurback.business.patient.dto.PatientDoctorInfo;
 import ee.valiit.ravimivalvurback.domain.Status;
+import ee.valiit.ravimivalvurback.domain.user.contact.Contact;
+import ee.valiit.ravimivalvurback.domain.user.contact.ContactMapper;
+import ee.valiit.ravimivalvurback.domain.user.contact.ContactRepository;
 import ee.valiit.ravimivalvurback.domain.user.doctor.DoctorPatient;
 import ee.valiit.ravimivalvurback.domain.user.doctor.DoctorPatientMapper;
 import ee.valiit.ravimivalvurback.domain.user.doctor.DoctorPatientRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -15,11 +20,19 @@ public class PatientService {
 
     private final DoctorPatientRepository doctorPatientRepository;
     private final DoctorPatientMapper doctorPatientMapper;
+    private final ContactRepository contactRepository;
+    private final ContactMapper contactMapper;
+
 
     public PatientDoctorInfo findPatientPendingStatus(Integer patientId) {
         DoctorPatient patientByPendingStatus = doctorPatientRepository.findPatientByPendingStatus(patientId, Status.PENDING);
-        PatientDoctorInfo patientDoctorInfo = doctorPatientMapper.toPatientDoctorInfo(patientByPendingStatus);
+       PatientDoctorInfo patientDoctorInfo = doctorPatientMapper.toPatientDoctorInfo(patientByPendingStatus);
 
+        for (PatientDoctorInfo doctorInfo : patientDoctorInfo) {
+            Contact contact = contactRepository.findDoctorBy(doctorInfo.getDoctorId());
+            doctorInfo.setFirstName(contact.getFirstName());
+            doctorInfo.setLastName(contact.getLastName());
+        }
         return patientDoctorInfo;
     }
 }
--- src/main/java/ee/valiit/ravimivalvurback/domain/user/contact/ContactRepository.java
+++ src/main/java/ee/valiit/ravimivalvurback/domain/user/contact/ContactRepository.java
@@ -2,6 +2,7 @@ package ee.valiit.ravimivalvurback.domain.user.contact;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,4 +14,9 @@ public interface ContactRepository extends JpaRepository<Contact, Integer> {
 
     @Query("select c from Contact c where c.user.role.name = :roleName order by c.lastName")
     List<Contact> findContactsByRole(String roleName);
+
+    @Query("select c from Contact c where c.user.id = :id and c.firstName = :firstName and c.lastName = :lastName")
+    Contact findDoctorBy(Integer id);
+
+
 }
