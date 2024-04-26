3c6cbfacd11ff10bafb10948b79d370ba6e9eb3a
Andres <andres.simuland@gmail.com>
2024-04-26 16:26:00 +0300

    Patsiendi aktsepteerimise teenus tehtud

--- src/main/java/ee/valiit/ravimivalvurback/business/patient/PatientService.java
+++ src/main/java/ee/valiit/ravimivalvurback/business/patient/PatientService.java
@@ -36,29 +36,17 @@ public class PatientService {
 
     @Transactional
     public void patientDoctorRelationshipResponse(Integer doctorPatientId, Boolean hasAccepted) {
+        DoctorPatient doctorPatient = doctorPatientRepository.getReferenceById(doctorPatientId);
         User user = userRepository.getReferenceById(doctorPatientId);
         user.setStatus(Status.ACTIVE);
         userRepository.save(user);
-        DoctorPatient doctorPatient = doctorPatientRepository.getReferenceById(doctorPatientId);
-        if (hasAccepted.booleanValue()) {
+
+        if (hasAccepted) {
             doctorPatient.setStatus(Status.ACTIVE);
             doctorPatientRepository.save(doctorPatient);
         }else{
             doctorPatientRepository.delete(doctorPatient);
         }
-
-        // todo: implemeteeri teenus
-        // todo: otsi üles doctorPatientId abil üles doctor_patient rida kui objekt 'doctorPatient'
-        // todo: 'doctorPatient' objekti küljes on ka olemas 'user' objekt
-        // todo: muuda ära 'user' staatus "A"-ks
-        // todo: salvesta userRepository abil ära 'user'
-        //  kui hasAccepted on true, siis muuta ära 'doctorPatient' staatus "A"-ks
-        // todo: salvesta doctorPatienRepository abil ära 'doctorPatient'
-        // todo: else
-        // todo: kustuta doctorPatienRepository abil ära 'doctorPatient'
-        // todo: doctorPatienRepository.delete(doctorPatient)
-
-
     }
 
 
